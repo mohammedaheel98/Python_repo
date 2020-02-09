31a2a59e515a23818423b8b901c581a70668fb20
Mohammed Aheel S <60208602+mohammedaheel98@users.noreply.github.com>
2020-02-09 16:32:00 +0530

    Create hangman game.py

---
+++ python/hangman game.py
@@ -0,0 +1,24 @@
+import random
+my_list = ["good","hello","world"]
+random.shuffle(my_list)
+print(my_list[0])
+hello
+answer = my_list[0]
+li = []
+li.append(answer)
+u=len(answer)
+print(u)
+5
+s=0
+while s<len(answer):
+    e=input()
+    if e in answer:
+        print(e)
+        break
+    else:
+        print("no")
+s=s+1
+good
+no
+hello
+hello
