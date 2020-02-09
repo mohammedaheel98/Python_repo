07731f7f87647b2a0ca4aeb30c0d51076b63ff61
Mohammed Aheel S <60208602+mohammedaheel98@users.noreply.github.com>
2020-02-09 16:26:21 +0530

    Update number guessing.py

--- python/number guessing.py
+++ python/number guessing.py
@@ -1,1 +1,11 @@
-
+import random
+a = random.randint(0,10)
+print(a)
+2
+user_input = int(input("enter the value: "))
+enter the value: 5
+if (user_input == a):
+    print("matched")
+else:
+    print("not matched")
+not matched
