757287e0d6fcdf1f4abc7f4a9f80f1a6b89bacce
Mohammed Aheel S <60208602+mohammedaheel98@users.noreply.github.com>
2020-02-09 16:27:30 +0530

    Create quiz.py

---
+++ python/quiz.py
@@ -0,0 +1,26 @@
+print("general quiz!!")
+question_1 ="who is CEO of google?"
+options = "a.myself\nb.aheel\nc.kannan\nd.sundarpichai"
+print(question_1)
+print(options)
+general quiz!!
+who is CEO of google?
+a.myself
+b.aheel
+c.kannan
+d.sundarpichai
+i=0
+while (i<=1):
+    response =input("hit 'a' 'b' 'c' or 'd' for your answer\n")
+    if response == "d":
+        print("voila!!")
+        break
+    else:
+        print("incorrect")
+    i=i+1
+hit 'a' 'b' 'c' or 'd' for your answer
+a
+incorrect
+hit 'a' 'b' 'c' or 'd' for your answer
+b
+incorrect
